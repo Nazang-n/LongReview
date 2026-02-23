c2b3b96178ba57ebe0df1125099ba595d884fd08
AMarNaUndartaker <settdragon@gmail.com>
2026-02-23 17:31:59 +0700

    revert: remove keep-alive ping, using UptimeRobot instead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +6,6 @@ import { HttpClient } from '@angular/common/http';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'LongReview';
-  private readonly BACKEND_URL = 'https://longreview.onrender.com';
-  private readonly PING_INTERVAL = 5 * 60 * 1000; // 5 minutes
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    // Keep backend alive by pinging every 5 minutes
-    setInterval(() => {
-      this.http.get(`${this.BACKEND_URL}/health`).subscribe({
-        error: () => { } // Silently ignore errors
-      });
-    }, this.PING_INTERVAL);
-  }
 }
